719db00aa6ec3f87dfc635e1bf0091768bd69acf
nicolaspc-dev <nicolaspc.wd@gmail.com>
2023-03-16 12:08:22 -0500

    Binding to Custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-
-  
+  serverElements = [{ type: 'server', name: 'Test', content: 'Just a test' }];
 }
